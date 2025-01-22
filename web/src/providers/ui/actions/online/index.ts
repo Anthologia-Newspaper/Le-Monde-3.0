622ddb87401290d1df6d635da29080f37e9af0f1
Adrien Fort <adrien.fort01@gmail.com>
2025-01-22 17:38:09 +0100

    rm useless todo

--- web/src/providers/ui/actions/online/index.ts
+++ web/src/providers/ui/actions/online/index.ts
@@ -4,7 +4,6 @@ import auth from './auth';
 import topics from './topics';
 import user from './user';
 
-// TODO: verify that all call have the right settings to show toast (and right messages / subMessages)
 const online = {
 	auth,
 	user,
