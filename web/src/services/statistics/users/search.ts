5583f7af882cdc729ba63a928e44717d8635ef8a
Antoine Gavira <antoine.gavira-bottari@epitech.eu>
2025-02-04 14:24:29 +0100

    fix(api): fix route for user statistics

--- web/src/services/statistics/users/search.ts
+++ web/src/services/statistics/users/search.ts
@@ -1,4 +1,4 @@
 import { servicesURL } from 'services';
 import { Stats } from 'types/statistics';
 
-export const oneUser = async (id: number) => servicesURL.get<Stats>(`/user/articles/${id}`);
+export const oneUser = async (id: number) => servicesURL.get<Stats>(`/statistics/users/${id}`);
